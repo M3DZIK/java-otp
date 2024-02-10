5a9f38fff3567a68efbe0a054c7f032dee422922
M3DZIK <me@medzik.dev>
2024-02-10 13:14:30 +0100

    Add test that check TOTP codes

--- src/test/java/dev/medzik/otp/TOTPGeneratorTests.java
+++ src/test/java/dev/medzik/otp/TOTPGeneratorTests.java
@@ -2,6 +2,7 @@ package dev.medzik.otp;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TOTPGeneratorTests {
@@ -11,11 +12,23 @@ public class TOTPGeneratorTests {
                 .type(OneTimePasswordType.TOTP)
                 .secret(OneTimePasswordParameters.Secret.generate())
                 .label(new OneTimePasswordParameters.Label("test"))
-                .issuer(new OneTimePasswordParameters.Issuer("test"))
                 .build();
 
         String code = TOTPGenerator.now(params);
 
         assertNotNull(code);
+    }
+
+    @Test
+    public void testTOTP() {
+        OneTimePasswordParameters params = OneTimePasswordParameters.builder()
+                .type(OneTimePasswordType.TOTP)
+                .secret(new OneTimePasswordParameters.Secret("JBSWY3DPEHPK3PXP"))
+                .label(new OneTimePasswordParameters.Label("test"))
+                .build();
+
+        assertEquals(TOTPGenerator.at(params, 1707566984), "785021");
+        assertEquals(TOTPGenerator.at(params, 1707567150), "342204");
+        assertEquals(TOTPGenerator.at(params, 1707567162), "342204");
     }
 }
